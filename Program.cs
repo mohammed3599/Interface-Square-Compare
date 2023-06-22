905ba252debdf278029ad0afca563e0929132529
mohammed3599 <kataloni1999@gmail.com>
2023-06-22 12:11:52 +0400

    Compare Function

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Interfce_Compare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(3);
+            Square square1 = new Square(2);
+
+            Console.WriteLine(square.CompareTo(square1));
+        }
+    }
+}
